b480d78f3d6d0089b02e629f056ac45c355d7648
OktayHalitEkinay <67113059+OktayHalitEkinay@users.noreply.github.com>
2023-01-05 22:39:08 +0300

    aa

---
+++ src/Application/Features/Commands/AddOrder.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.AggregateModels.OrderModels;
+using MediatR;
+using Shared.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Commands
+{
+    public class AddOrder : IRequest<IDataResult<Order>>
+    {
+        Order order;
+
+        public class AddOrderHandler : IRequestHandler<AddOrder, IDataResult<Order>>
+        {
+            private readonly IOrderRepository _orderRepository;
+
+            public AddOrderHandler(IOrderRepository orderRepository)
+            {
+                _orderRepository = orderRepository;
+            }
+            public async Task<IDataResult<Order>> Handle(AddOrder request, CancellationToken cancellationToken)
+            {
+                Order result = _orderRepository.AddAsync(request.order);
+                return new SuccessDataResult<Order>(result);
+            }
+        }
+    }
+}
---
+++ src/Presentation/WebApi/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrdersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet("AddAsync")]
+        public async Task<IActionResult> AddAsync()
+        {
+            var query = new GetAllProductsQuery();
+            return Ok(await _mediator.Send(query));
+        }
+
+    }
+}
